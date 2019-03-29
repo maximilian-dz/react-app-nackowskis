33e3915fddc2c5a506212a579716e134d949a92f
Ludwig <ludde-j@hotmail.com>
2019-03-29 11:41:44 +0100

    jadu inte mkt

--- src/components/auction/AuctionList.js
+++ src/components/auction/AuctionList.js
@@ -1,1 +1,12 @@
-let hello = "hej";
+import React, { Component } from 'react';
+import AuctionSummary from './AuctionSummary.js';
+
+export default class Auctionlist extends Component
+{
+    render()
+    {
+        let Auctions = "";
+
+        return (<div>{Auctions}</div>)
+    }
+}
