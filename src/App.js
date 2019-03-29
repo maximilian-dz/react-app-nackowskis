09f47fd11a359aa14cdc35f4f4e2cbc57b24cba8
Maximilian <max-dz@hotmail.com>
2019-03-29 11:04:29 +0100

    added routes/state to App+Search/filter function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import AuctionDetails from './components/auction/AuctionDetails';
+import Search from './components/dashboard/Search';
 
 class App extends Component {
+  state = {
+    auctions: [],
+    filtered: []
+  };
+  componentWillMount() {
+    // const auctions = fetchAuctions();
+    // this.setState({
+    //   auctions,
+    //   filtered: auctions
+    // })
+  }
+  handleChange = (val) => {
+    let currentList = this.state.auctions;
+    let newList = [];
+
+    if (val !== '' && val !== undefined) {
+      newList = currentList.filter((auction) =>
+        auction.Titel.toLowerCase().includes(val)
+      );
+    } else {
+      newList = currentList;
+    }
+
+    this.setState({
+      filteredAuctions: newList
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <h1>NACKOWSKIS</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Search />
+        </div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <Dashboard
+                {...props}
+                isAuthed={true}
+                auctions={this.state.filtered}
+              />
+            )}
+          />
+          <Route
+            path="/auction/:AuktionID/"
+            render={(props) => (
+              <AuctionDetails
+                {...props}
+                isAuthed={true}
+                auctions={this.state.auctions}
+              />
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/dashboard/Search.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  handleChange = (e) => {
+    this.props.onChange(e.target.value);
+  };
+  render() {
+    return (
+      <div className="container search lighten-1 no-shadow">
+        <div className="nav-wrapper">
+          <form>
+            <div className="input-field">
+              <input
+                className="transparent"
+                id="search"
+                type="search"
+                required
+                onChange={this.handleChange}
+              />
+              <label className="label-icon" htmlFor="search">
+                <i className="material-icons">search</i>
+              </label>
+              <i className="material-icons">close</i>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
