34f7f33a6f196921c1dfd92b060bf741cde30736
tombergqvist <bergqvist.tom@gmail.com>
2019-03-29 11:48:18 +0100

    post WIP

--- src/components/API/WebAPI.js
+++ src/components/API/WebAPI.js
@@ -5,13 +5,27 @@ export async function fetchData(url) {
 }
 
 export async function getAllAuctions(group) {
-    return fetchData("http://nackowskis.azurewebsites.net/api/Auktion/"+ group)
+    return fetchData("http://nackowskis.azurewebsites.net/api/Auktion/" + group)
 }
 
 export async function getAuction(group, auction) {
-    return await fetchData("http://nackowskis.azurewebsites.net/api/Auktion/"+ group +"/"+ auction)
+    return await fetchData("http://nackowskis.azurewebsites.net/api/Auktion/" + group + "/" + auction)
 }
 
-export async function getBids(group, auction){
-    return await fetchData("http://nackowskis.azurewebsites.net/api/bud/"+ group +"/"+ auction)
+export async function getBids(group, auction) {
+    return await fetchData("http://nackowskis.azurewebsites.net/api/bud/" + group + "/" + auction)
+}
+
+// Warning! This is a work in progress... Do not use this yet! :)
+export function post(group, auction, json) {
+    fetch("http://nackowskis.azurewebsites.net/api/auktion/"+ group +"/"+ auction, {
+        method: 'POST',
+        body: JSON.stringify(json),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    }).then(function () {
+        console.log('POST successful.')
+    })
 }
