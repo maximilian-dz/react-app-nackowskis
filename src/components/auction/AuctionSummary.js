fb755dc7dbb2849917624020900d0cb924bead2d
Ludwig <ludde-j@hotmail.com>
2019-03-29 15:50:28 +0100

    venne

--- src/components/auction/AuctionSummary.js
+++ src/components/auction/AuctionSummary.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-const AuctionSummary = ({ auction }) => 
-{
+const AuctionSummary = ({ auction }) => {
     return (<div className="App">
-        <div className="AuctionBox">
-            <div className="row">
-                <div className="col s12 m6">
-                    <div className="card blue-grey darken-1">
-                        <div className="card-content white-text">
-                            <span className="card-title">{auction.Titel}</span>
-                            <p>{auction.Beskrivning}</p>
-                            <br></br>
-                            <p>{auction.StartDatum} &nbsp; {auction.SlutDatum}</p>
-                        </div>
+        <div className="row">
+            <div className="col s12 m6">
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">{auction.Titel}</span>
+                        <p>{auction.Beskrivning}</p>
+                        <br></br>
+                        <p>{auction.StartDatum} &nbsp; {auction.SlutDatum}</p>
                     </div>
                 </div>
             </div>
