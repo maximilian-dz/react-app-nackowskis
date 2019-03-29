da7e5c6d34c3d0c643f8377f19d45538825085fa
Ludwig <ludde-j@hotmail.com>
2019-03-29 15:19:46 +0100

    styling

--- src/components/auction/AuctionSummary.js
+++ src/components/auction/AuctionSummary.js
@@ -1,29 +1,23 @@
 import React from 'react';
 
-const AuctionSummary = ({ auction }) =>
+const AuctionSummary = ({ auction }) => 
 {
-        // return(<div className="App">
-        //     <h3>{ auction.Titel }</h3>
-        //     <p>{ auction.Beskrivning }</p>
-        //     <p>{ auction.SlutDatum }</p>
-        //     <p>{ auction.SlutDatum }</p>
-        //     <button> Open </button>
-        // </div>);
-
-        return(<div className="App">
-        <div class="row">
-    <div class="col s12 m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">{ auction.Titel }</span>
-          <p>{ auction.Beskrivning }</p>
-          <br></br>
-          <p>{ auction.StartDatum } &nbsp; { auction.SlutDatum }</p>
+    return (<div className="App">
+        <div className="AuctionBox">
+            <div className="row">
+                <div className="col s12 m6">
+                    <div className="card blue-grey darken-1">
+                        <div className="card-content white-text">
+                            <span className="card-title">{auction.Titel}</span>
+                            <p>{auction.Beskrivning}</p>
+                            <br></br>
+                            <p>{auction.StartDatum} &nbsp; {auction.SlutDatum}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-        </div>)
+    </div>)
 }
 
 export default AuctionSummary;
