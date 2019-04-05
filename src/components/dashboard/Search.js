6ecf9448ec005445d71db6ba4b5ac7993434ceb5
Maximilian <max-dz@hotmail.com>
2019-04-05 11:16:24 +0200

    Clicking close icon will now empty input field

--- src/components/dashboard/Search.js
+++ src/components/dashboard/Search.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
 class Search extends Component {
+  state = {
+    input: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
   handleSubmit = (e) => {
     e.preventDefault()
-    this.props.onSubmit(this.textInput.value)
+    this.props.onSubmit(this.state.input)
     this.props.history.push('/')
+  }
+  handleCloseClick = () => {
+    this.setState({
+      input: ''
+    })
   }
   render() {
     return (
@@ -17,13 +30,16 @@ class Search extends Component {
                 className="transparent "
                 id="search"
                 type="search"
-                ref={(input) => (this.textInput = input)}
+                onChange={this.handleChange}
                 placeholder="Search auction..."
+                value={this.state.input}
               />
               <label className="label-icon" htmlFor="search">
                 <i className="material-icons">search</i>
               </label>
-              <i className="material-icons">close</i>
+              <i className="material-icons" onClick={this.handleCloseClick}>
+                close
+              </i>
             </div>
           </form>
         </div>
