569b905e7994e871d4304d8fe8d48534e3fa46fe
Sara Björnlund <sara_bjornlund@hotmail.com>
2019-04-05 13:23:04 +0200

    changed format startdate

--- src/components/auction/CreateAuction.js
+++ src/components/auction/CreateAuction.js
@@ -12,7 +12,7 @@ export default class CreateAuction extends Component {
       StartDatum: moment().format('YYYY-MM-DDTHH:MM:SS'),
       SlutDatum: undefined,
       Utropspris: undefined,
-      SkapadAv: undefined
+      SkapadAv: undefined,
     }
     this.datepicker = React.createRef()
   }
@@ -20,7 +20,7 @@ export default class CreateAuction extends Component {
   componentDidMount = () => {
     const options = {
       onSelect: this.handleDateChange,
-      autoClose: true
+      autoClose: true,
     }
     M.Datepicker.init(this.datepicker.current, options)
   }
@@ -29,8 +29,9 @@ export default class CreateAuction extends Component {
     e.preventDefault()
 
     this.setState({
-      SkapadAv: this.SkapadAv + "<img src='" + this.refs.img.value +  "'></img>"
+      Beskrivning: this.Beskrivning + "<img src='" + this.refs.img.value +  "'></img>"
     })
+    console.log(this.refs.img.value);
 
     this.props.onSubmit(this.state)
     this.props.history.push('/')
@@ -84,7 +85,7 @@ export default class CreateAuction extends Component {
                 type="text"
                 id="StartDatum"
                 name="StartDatum"
-                value={moment().format('YYYY-MM-DD')}
+                value={moment().format('MMM DD, YYYY')}
                 readOnly
               />
             </div>
