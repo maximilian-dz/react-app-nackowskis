d4deac98a4b7ea1a39d86953ecb756b00e09b426
Maximilian <max-dz@hotmail.com>
2019-04-06 12:34:30 +0200

    added some styling to summary

--- src/components/auction/AuctionSummary.js
+++ src/components/auction/AuctionSummary.js
@@ -3,7 +3,8 @@ import moment from 'moment'
 
 class AuctionSummary extends Component {
   state = {
-    bid: ''
+    bid: '',
+    bidCount: ''
   }
   componentDidMount = () => {
     const { bids } = this.props
@@ -13,54 +14,60 @@ class AuctionSummary extends Component {
           return bid.Summa > max ? bid.Summa : max
         }, 0)
         this.setState({
-          bid
+          bid,
+          bidCount: res.length
         })
       })
     }
   }
 
+  getBadge = () => {
+    const { auction } = this.props
+    const { bid } = this.state
+    let badge = ''
+
+    if (Date.parse(auction.SlutDatum) < Date.now()) {
+      badge = 'Auction Ended'
+    } else if (bid !== 0) {
+      badge = 'BID: ' + auction.Utropspris + ' SEK'
+    } else {
+      badge = 'ESTIMATE: ' + this.state.bid + ' SEK'
+    }
+    return badge
+  }
+
   render() {
     const { auction } = this.props
-    const startDate = moment(auction.StartDatum).format('ddd Do MMM, hh:mm')
     const endDate = moment(auction.SlutDatum).format('ddd Do MMM, hh:mm')
+    const badge = this.getBadge()
+
+    const content =
+      badge === 'Auction Ended' ? (
+        <span className="card-title summary-bid bid-color-alt">{badge}</span>
+      ) : (
+        <span className="card-title summary-bid bid-color">{badge}</span>
+      )
+
     return (
-      <div className="App">
-        <div className="limited-width">
-          <div className="col s12 m5">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  src="http://therightsofnature.org/wp-content/uploads/2018/01/turkey-3048299_1920.jpg"
-                  alt="auction-pic"
-                />
-                <div className="card-content">
-                  <h5 className="title">{auction.Titel}</h5>
-                  <p>Nuvarande bud: {this.state.bid}</p>
-                  <p>Utropspris: {auction.Utropspris}</p>
-                  <p>Startdatum: {startDate}</p>
-                  <p>Slutdatum: {endDate}</p>
-                </div>
-              </div>
+      <div>
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image center summary-img">
+              <img
+                className="summary-img-width"
+                src="https://cdn.shopify.com/s/files/1/1245/1481/products/Classic_Plus_Side_Profile_web_1024x1024.jpg?v=1531164809"
+                alt="auction-pic"
+              />
+              <span className="card-title summary-title">{auction.Titel}</span>
+              {content}
+            </div>
+            <div className="card-action summary-content">
+              <p>Bids: {this.state.bidCount}</p>
+              <p>Auction Ends: {endDate}</p>
             </div>
           </div>
         </div>
       </div>
-      // {<div className="limited-width">
-      //     <div className="col s12 m6">
-      //         <div className="card blue-grey darken-1">
-      //             <div className="card-content white-text">
-      //                 <span className="card-title">{auction.Titel}</span>
-      //                 <h5 className="current-bid">
-      //                     Nuvarande bud: {auction.Utropspris}
-      //                 </h5>
-      //                 <br />
-      //                 <p className="auctions-dates">
-      //                     {auction.StartDatum}&nbsp;- {auction.SlutDatum}
-      //                 </p>
-      //             </div>
-      //         </div>
-      //     </div>
-      // </div>}
     )
   }
 }
